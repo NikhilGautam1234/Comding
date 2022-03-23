8bbd51c90105226e1f56420ce51cdc622cf12b04
NikhilGautam1234 <nikhilgautam55863@gmail.com>
2022-03-23 10:12:47 +0530

    Saksham(Count vowels and consonants)

--- C/test.c
+++ C/test.c
@@ -1,6 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+
+void count(char s[10001]){
+    int v=0,c=0,sp=0;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if(s[i]=='a'||s[i]=='A'||s[i]=='e'||s[i]=='E'||s[i]=='i'||s[i]=='I'||s[i]=='o'||s[i]=='O'||s[i]=='u'||s[i]=='U') v++;
+        else if(s[i]==' '||s[i]=='b'||s[i]=='B') sp++;
+        else c++;
+    }
+    printf("%d\n%d\n%d\n",v,c,sp);
+}
 
 int main(void){
-    
+    char sen[10001];
+    scanf("%[^\n]",&sen);
+    count(sen);
 }
