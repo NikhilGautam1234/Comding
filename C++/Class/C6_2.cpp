b3e689efe06654d5896102afdcd3ed60fc6cb3f2
NikhilGautam1234 <nikhilgautam55863@gmail.com>
2022-04-04 15:21:46 +0530

    Operator Overloading

--- C++/Class/C6_2.cpp
+++ C++/Class/C6_2.cpp
@@ -1,7 +1,27 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+class zoro{
+    int a,b,c;
+    public:
+    void inp(int x,int y,int z){
+        a=x;b=y;c=z;
+    }
+    friend zoro operator -(zoro enma){
+        enma.a=--enma.a;
+        enma.b=--enma.b;
+        enma.c=--enma.c;
+        return enma;
+    }
+    void oup(){
+        cout<<a<<" "<<b<<" "<<c<<endl;
+    }
+};
+
 int main(){
-    
+    zoro A,c,b;
+    A.inp(1,-2,3);
+    c=operator -(A);
+    c.oup();
     return 0;
 }
